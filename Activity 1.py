0a514c1774d05d296f8651a6368acf132770fa74
Hamza7655code <mughis.rehman@yahoo.com>
2025-04-24 18:03:51 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+pen = turtle.Turtle()
+
+sides = 6
+sideLength = 100
+angle = 360/sides
+for i in  range(sides):
+    pen.forward(sideLength)
+    pen.right(angle)
+
+turtle.done()
+    
+   
+
+
