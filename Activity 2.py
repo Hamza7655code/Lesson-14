761384f6945fcebc13b5b0c4307206501423eac0
Hamza7655code <mughis.rehman@yahoo.com>
2025-04-24 18:15:29 +0100

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,30 @@
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+pen = turtle.Turtle()
+
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+
+pen.penup()
+
+pen.right(150)
+pen.forward(50)
+
+pen.pendown()
+
+pen.right(90)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+pen.right(120)
+pen.forward(100)
+
+turtle.done()
+
+
